edf78f64e81b48e1ad36a453c12e9b2301d4b474
shawnkoon <shawn9405@gmail.com>
2017-05-27 23:49:10 -0700

    Added Task class & renders tasks in tableview along with important item distinction

---
+++ todokoon/todokoon/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  todokoon
+//
+//  Created by Shawn Son on 5/27/17.
+//  Copyright © 2017 shawnkoon. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = true
+}
--- todokoon/todokoon/ViewController.swift
+++ todokoon/todokoon/ViewController.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tasks: [Task] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tasks = makeTasks()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        if task.important {
+            cell.textLabel?.text = "‼️\(task.name)"
+        } else {
+            cell.textLabel?.text = task.name
+        }
+        
+        return cell
+    }
+    
+    func makeTasks() -> [Task] {
+        let task1 = Task()
+        task1.name = "Code program"
+        task1.important = false
+        
+        let task2 = Task()
+        task2.name = "Cut the cheese"
+        task2.important = true
+        
+        let task3 = Task()
+        task3.name = "Wash my car"
+        task3.important = true
+        
+        return [task1, task2, task3]
+    }
 }
 
